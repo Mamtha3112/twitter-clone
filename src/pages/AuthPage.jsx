85c71f0dc9521de7e8e5c306e9fb88d442d2ab07
Mamtha <mamthasai96@gmail.com>
2025-07-03 10:57:21 +0800

    add facebook button

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -85,6 +85,10 @@ export default function AuthPage() {
                     <Button className="rounded-pill" variant="outline-dark">
                         <i className="bi bi-apple"></i> Sign up with Apple
                     </Button>
+                    <Button className="rounded-pill" variant="outline-dark">
+                        <i className="bi bi-facebook"></i>
+                        Sign Up with Facebook
+                    </Button>
                     <p style={{ textAlign: "center" }}>or</p>
                     <Button className="rounded-pill" onClick={handleShowSignUp}>
                         Create an account
